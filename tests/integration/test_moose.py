4b742780a96c703921b5ff8bd5fb8c61b1eb25eb
Matt <matthew.field@ukaea.uk>
2026-06-23 15:34:34 +0100

    fix patching

--- tests/integration/test_moose.py
+++ tests/integration/test_moose.py
@@ -24,11 +24,11 @@ def run_moose(
     with tempfile.TemporaryDirectory() as tempd:
         temp_input_file = pathlib.Path(tempd).joinpath(moose_file_path.name)
         input_file_data = moose_file_path.read_text()
-        input_file_data.replace(
+        patched_file_data = input_file_data.replace(
             "$OUTPUT_PATH",
             str(pathlib.Path(tempd).joinpath("results")) + "simvue_thermal",
         )
-        temp_input_file.write_text(input_file_data)
+        temp_input_file.write_text(patched_file_data)
 
         # Initialise the MooseRun class as a context manager
         with MooseRun(mode="offline" if offline else "online") as run:
@@ -53,8 +53,8 @@ def run_moose(
 
             if load:
                 run.load(
-                    moose_file_path=temp_input_file,
-                    results_dir_path=tempd.joinpath("results"),
+                    moose_file_path=moose_file_path,
+                    results_dir_path=moose_load_path,
                     # You can optionally choose to track VectorPostProcessor outputs too:
                     track_vector_postprocessors=True,
                     track_vector_positions=False,
