c8798367896a80fc2d7cfcca3cdd93f34c5c00d9
Matt <matthew.field@ukaea.uk>
2026-07-29 10:05:42 +0100

    Address PR comments, move workdir_path back to being optional

--- simvue_moose/connector.py
+++ simvue_moose/connector.py
@@ -85,7 +85,7 @@ class MooseRun(WrappedRun):
         """
         self.moose_application_path: pydantic.FilePath = None
         self.moose_file_path: pydantic.FilePath = None
-        self.workdir_path: pathlib.Path = pathlib.Path.cwd()
+        self.workdir_path: pathlib.Path | None = None
         self.upload_files: list[str] | None = None
         self.track_vector_postprocessors: bool = None
         self.track_vector_positions: bool = None
@@ -134,6 +134,8 @@ class MooseRun(WrappedRun):
             The file prefix which results will be generated with
 
         """
+        workdir = self.workdir_path if self.workdir_path else pathlib.Path.cwd()
+
         if not self._file_base:
             # Uses directory containing input file, with moose file name stem as prefix
             return self.moose_file_path.parent, self.moose_file_path.stem
@@ -141,8 +143,12 @@ class MooseRun(WrappedRun):
         # Try splitting on final slash
         split = self._file_base.rsplit("/", maxsplit=1)
         # If not split, no slashes, so only contains file prefix
+        # It then puts things in the *working directory*, not input file parent
         if len(split) < 2:
-            return self.workdir_path, split[0]
+            return (
+                workdir,
+                split[0],
+            )
 
         dir_path, results_prefix = split
 
@@ -154,7 +160,7 @@ class MooseRun(WrappedRun):
                 )
             return pathlib.Path(dir_path), results_prefix
         # Otherwise, relative path, should be relative to working dir
-        return self.workdir_path.joinpath(dir_path).resolve(), results_prefix
+        return workdir.joinpath(dir_path).resolve(), results_prefix
 
     def _moose_input_parser(self, input_file: pathlib.Path) -> dict[str, typing.Any]:
         """Parse MOOSE input file, and create a dictionary of metadata with dot notation representing indentation of keys.
@@ -464,18 +470,18 @@ class MooseRun(WrappedRun):
             pattern=" Solve Did NOT Converge",
             notification="email",
         )
+        if self.workdir_path:
+            # Ensure workdir path exists
+            self.workdir_path.mkdir(parents=True, exist_ok=True)
 
-        # Ensure workdir path exists
-        self.workdir_path.mkdir(parents=True, exist_ok=True)
-
-        # If not cwd, create copy of input file into this path
-        if self.workdir_path.resolve() != pathlib.Path.cwd().resolve():
+            # If not cwd, create copy of input file into this path
             moose_file_copy = self.workdir_path.joinpath(self.moose_file_path.name)
-            shutil.copy(
-                self.moose_file_path,
-                moose_file_copy,
-            )
-            self.moose_file_path = moose_file_copy
+            if self.moose_file_path.resolve() != moose_file_copy.resolve():
+                shutil.copy(
+                    self.moose_file_path,
+                    moose_file_copy,
+                )
+                self.moose_file_path = moose_file_copy
 
         # Save the MOOSE file for this run to the Simvue server
         if pathlib.Path(self.moose_file_path).exists() and (
@@ -663,9 +669,7 @@ class MooseRun(WrappedRun):
 
         self.moose_application_path = moose_application_path
         self.moose_file_path = moose_file_path
-        self.workdir_path = (
-            pathlib.Path(workdir_path) if workdir_path else pathlib.Path.cwd()
-        )
+        self.workdir_path = pathlib.Path(workdir_path) if workdir_path else None
         self.upload_files = upload_files
         self.track_vector_postprocessors = track_vector_postprocessors
         self.track_vector_positions = track_vector_positions
@@ -709,6 +713,8 @@ class MooseRun(WrappedRun):
         ------
         ValueError
             Raised if conflicting values of track_vector_positions and track_vector_postprocessors are found.
+        FileNotFoundError
+            Raised if no results directory is found at expected location
 
         """
         if track_vector_positions and not track_vector_postprocessors:
@@ -723,9 +729,7 @@ class MooseRun(WrappedRun):
         self._loading_historic_run = True
 
         # Save the MOOSE file for this run to the Simvue server
-        if pathlib.Path(self.moose_file_path).exists() and (
-            self.upload_files is None or self.moose_file_path.name in self.upload_files
-        ):
+        if self.upload_files is None or self.moose_file_path.name in self.upload_files:
             self.save_file(self.moose_file_path, category="input")
 
         # Parse the MOOSE input file
@@ -734,8 +738,20 @@ class MooseRun(WrappedRun):
         # Extract useful information
         self._moose_input_callback(input_metadata)
 
-        if results_dir and results_dir.resolve() != self._output_dir_path.resolve():
+        if results_dir:
+            if (
+                self._file_base
+                and results_dir.resolve() != self._output_dir_path.resolve()
+            ):
+                print(
+                    f"Warning: File base specified in input file, '{self._file_base}', conflicts with user provided results_dir path. The path in the MOOSE input file will be ignored."
+                )
             self._output_dir_path = results_dir
+
+        if not self._output_dir_path.exists():
+            raise FileNotFoundError(
+                f"No results directory found at '{self._output_dir_path}'!"
+            )
 
         log_path = self._output_dir_path.joinpath(
             f"{self._results_prefix}.txt"
