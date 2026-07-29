78768b9423b74881dbb5b794a1aca866f127a85c
Matt <matthew.field@ukaea.uk>
2026-07-29 15:16:25 +0100

    Fix tests to look at .out file

--- tests/unit/test_moose_log_parser.py
+++ tests/unit/test_moose_log_parser.py
@@ -1,45 +1,9 @@
 from simvue_moose.connector import MooseRun
 import simvue
-import threading
-import time
-import tempfile
-from unittest.mock import patch
 import uuid
 import pathlib
 
 
-def mock_moose_process(self, *_, **__):
-    """
-    Mock process which writes each entry in the example log file, line by line
-    """
-    temp_logfile = tempfile.NamedTemporaryFile(
-        mode="w", prefix="moose_test_", suffix=".txt", buffering=1
-    )
-    self._file_base = temp_logfile.name
-    self._results_prefix = pathlib.Path(temp_logfile.name).name.split(".")[0]
-    self._output_dir_path = pathlib.Path(temp_logfile.name).parent
-
-    def write_to_log():
-        log_file = (
-            pathlib.Path(__file__)
-            .parent.joinpath("example_data", "moose_log.txt")
-            .open("r")
-        )
-        for line in log_file:
-            temp_logfile.write(line)
-            time.sleep(0.01)
-        temp_logfile.flush()
-        time.sleep(1)
-        temp_logfile.close()
-        self._trigger.set()
-        return
-
-    thread = threading.Thread(target=write_to_log)
-    thread.start()
-
-
-@patch.object(MooseRun, "_moose_input_parser", lambda *_, **__: {})
-@patch.object(MooseRun, "add_process", mock_moose_process)
 def test_moose_log_parser(folder_setup):
     """
     Check that Events and Metrics are correctly parsed from the MOOSE log file and uploaded.
@@ -49,10 +13,16 @@ def test_moose_log_parser(folder_setup):
         run.config(disable_resources_metrics=True)
         run.init(name=name, folder=folder_setup)
         run_id = run.id
-        run.launch(
-            moose_application_path=pathlib.Path(__file__),
-            moose_file_path=pathlib.Path(__file__),
-        )
+
+        run.log_event("Beginning MOOSE simulation...")
+        for line in (
+            pathlib.Path(__file__)
+            .parent.joinpath("example_data", "moose_log.txt")
+            .open("r")
+        ):
+            run._log_parser(line)
+        run.log_event("Simulation complete!")
+
     client = simvue.Client()
     # Check messages correctly extracted from log and added as events
     events = client.get_events(run_id)
